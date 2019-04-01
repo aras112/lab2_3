a8dfbb2492333884ce9527f33e1c112527268c56
Caroolinnee <karolinab341@gmail.com>
2019-04-01 16:55:40 +0200

    v3_test first simple test added

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -17,6 +17,47 @@ public class SimilarityFinderTest
     @Before
     public void before()
         {
+        }
+
+    @Test
+    public void simpleTestWithThisSameValue()
+        {
+        final boolean[] booleanValueOfNextSearchResult = {true, true};
+        int[] tab1 = {1, 2};
+        int[] tab2 = {1, 2};
+
+        sequenceSearcher = getMockSearcher(booleanValueOfNextSearchResult);
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcher);
+
+        Assert.assertThat(similarityFinder.calculateJackardSimilarity(tab1, tab2), is(1D));
+
+        }
+
+    @Test
+    public void simpleTestWithSimilarValue()
+        {
+        final boolean[] booleanValueOfNextSearchResult = {true, true};
+        int[] tab1 = {1, 2};
+        int[] tab2 = {1, 2, 3};
+
+        sequenceSearcher = getMockSearcher(booleanValueOfNextSearchResult);
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcher);
+
+        Assert.assertThat(similarityFinder.calculateJackardSimilarity(tab1, tab2), is(2D / 3D));
+
+        }
+
+    @Test
+    public void simpleTestWithSimilarTabNowMoreValuesInFirstTab()
+        {
+        final boolean[] booleanValueOfNextSearchResult = {true, true, false};
+        int[] tab1 = {1, 2, 5};
+        int[] tab2 = {1, 2, 3};
+
+        sequenceSearcher = getMockSearcher(booleanValueOfNextSearchResult);
+        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcher);
+
+        Assert.assertThat(similarityFinder.calculateJackardSimilarity(tab1, tab2), is(2D / 4D));
 
         }
 
